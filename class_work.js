1421d1787980f410e29a325a46a3d1a9d5a7bb8d
kcho760 <kcho760@gmail.com>
2023-04-26 14:18:30 -0400

    print time

--- class_work.js
+++ class_work.js
@@ -1,19 +1,21 @@
 class Clock {
-    constructor(date, hours, minutes, seconds) {
+    constructor() {
       // 1. Create a Date object.
-      this.date = date;
+      const currentDate = new Date();
       // 2. Store the hours, minutes, and seconds.
-      this.hours = hours;
-      this.minutes = minutes;
-      this.seconds = seconds;
+      this.hours = currentDate.getHours();
+      this.minutes = currentDate.getMinutes();
+      this.seconds = currentDate.getSeconds();
       // 3. Call printTime.
-      function print
+      let time = printTime.call(currentDate, this.hours, this.minutes, this.seconds)
       // 4. Schedule the tick at 1 second intervals.
     }
   
     printTime() {
       // Format the time in HH:MM:SS
+      const formattedTime = `${this.hours},${this.minutes},${this.seconds}`;
       // Use console.log to print it.
+      console.log(formattedTime);
     }
   
     _tick() {
