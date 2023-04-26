8def71a0bb6303adf19c2ac233b0376877a672bb
Naoko Sugawara <infoseek122@gmail.com>
2023-04-26 00:39:40 -0400

    first commit

---
+++ class_work.js
@@ -0,0 +1,25 @@
+class Clock {
+    constructor(date, hours, minutes, seconds) {
+      // 1. Create a Date object.
+      this.date = date;
+      // 2. Store the hours, minutes, and seconds.
+      this.hours = hours;
+      this.minutes = minutes;
+      this.seconds = seconds;
+      // 3. Call printTime.
+      function print
+      // 4. Schedule the tick at 1 second intervals.
+    }
+  
+    printTime() {
+      // Format the time in HH:MM:SS
+      // Use console.log to print it.
+    }
+  
+    _tick() {
+      // 1. Increment the time by one second.
+      // 2. Call printTime.
+    }
+  }
+  
+  const clock = new Clock();
